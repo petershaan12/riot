ef7cf0e7979d5b77475e6b98da49824f82f31eb2
petershaan12 <2181002@unai.edu>
2024-11-14 16:41:52 +0700

    edit layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,4 +1,4 @@
-import { isOfficer } from "react";
+import { Suspense } from "react";
 import Loading from "../(root)/loading";
 
 export default function RootLayout({
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <div className="flex h-screen flex-col w-screen bg-gradasi">
       <main className="flex-1 z-10 justify-center flex flex-col  items-center ">
-        <isOfficer fallback={<Loading />}>{children}</isOfficer>
+        <Suspense fallback={<Loading />}>{children}</Suspense>
       </main>
     </div>
   );
